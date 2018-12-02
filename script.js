c7bcfa5deba3474d756c02f05070d6239d94de37
KosVoe <voedylo@bigmir.net>
2018-12-02 22:07:27 +0200

    HomeWork_02_12_18

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 "use strict";
-const adminLogin = prompt("Please enter Login:");
-if (2018) console.log(adminLogin);
+const adminLogin = "adm@gmail.com";
+const adminPassword = "adm123";
+
+const cancelLog = "canceled by user!";
+const wrongLog = "Access is denied!";
+
+const cancelPass = "canceled by user!";
+const wrongPass = "Access is denied!";
+
+const greet = "Welcome!";
+
+const logInput = prompt("please, enter your login", "");
+
+if (logInput === null) {
+  alert(cancelLog);
+} else if (logInput !== adminLogin) {
+  alert(wrongLog);
+} else {
+  const passInput = prompt("please, enter your password", "");
+
+  if (passInput === null) {
+    alert(cancelPass);
+  } else if (passInput !== adminPassword) {
+    alert(wrongPass);
+  } else {
+    alert(greet);
+  }
+}
